399ee5d2b710c796b0cfe9da2925dab4774bbe70
esosaphilip <esosaphilip@gmail.com>
2022-11-07 19:33:00 +0100

    Initial commit

---
+++ app/src/main/java/com/esosaphilip/dogapp/MainActivity.kt
@@ -0,0 +1,209 @@
+package com.esosaphilip.dogapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.esosaphilip.dogapp.model.Dog
+import com.esosaphilip.dogapp.model.dogs
+import com.esosaphilip.dogapp.ui.theme.DogAppTheme
+import com.esosaphilip.dogapp.ui.theme.Purple200
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DogAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    WoofApp()
+                }
+            }
+        }
+    }
+}
+@Composable
+fun DogHobby(@StringRes dogHobby: Int, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            top = 8.dp,
+            bottom = 16.dp,
+            end = 16.dp
+        )
+    ) {
+        Text(
+            text = stringResource(R.string.about),
+            style = MaterialTheme.typography.h3
+        )
+        Text(
+            text = stringResource(dogHobby),
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+@Composable
+fun DogItemButton( expanded: Boolean,
+                onClick: () -> Unit,
+                modifier: Modifier = Modifier) {
+
+    IconButton(onClick = onClick) {
+        Icon(
+            imageVector = if(expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+            tint = MaterialTheme.colors.secondary,
+            contentDescription = stringResource(R.string.expand_button_content_description)
+        )
+    }
+
+}
+
+
+@Composable
+fun WoofTopAppBar(modifier: Modifier = Modifier){
+    Row( modifier = modifier
+        .fillMaxWidth()
+        .background(color = MaterialTheme.colors.primary),
+            verticalAlignment = Alignment.CenterVertically){
+        Image(
+            modifier = Modifier
+                .size(64.dp)
+                .padding(8.dp),
+            painter = painterResource(R.drawable.ic_woof_logo),
+            contentDescription = null
+        )
+        Text(
+            text = stringResource(R.string.app_name),
+            style = MaterialTheme.typography.h1
+        )
+    }
+}
+
+@Composable
+fun WoofApp() {
+    Scaffold(
+        topBar = {
+            WoofTopAppBar()
+        }
+    ){
+        LazyColumn(modifier =
+        Modifier.background(MaterialTheme.colors.background)) {
+            items(dogs) {
+                DogItem(dog = it)
+            }
+        }
+    }
+}
+@Composable
+fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
+    
+    var expanded by remember { mutableStateOf(false) }
+
+    val color by animateColorAsState(
+        targetValue = if (expanded) Purple200 else MaterialTheme.colors.surface,
+
+    )
+
+   Card(modifier = modifier.padding(8.dp),elevation = 4.dp){
+       Column(modifier = Modifier
+           .animateContentSize(animationSpec = spring(
+               dampingRatio = Spring.DampingRatioMediumBouncy,
+               stiffness = Spring.StiffnessLow))
+           .background(color = color)){
+           Row(
+               modifier = Modifier
+                   .fillMaxWidth()
+                   .padding(8.dp)
+           ) {
+               DogIcon(dog.imageResourceId)
+               DogInformation(dog.name, dog.age)
+               Spacer(Modifier.weight(1f))
+               DogItemButton(expanded = expanded ,
+                   onClick = { expanded = !expanded})
+           }
+           if(expanded){
+               DogHobby( dog.hobbies)
+           }
+
+       }
+       
+   }
+}
+
+@Composable
+fun DogIcon(@DrawableRes dogIcon: Int, modifier: Modifier = Modifier) {
+        Image(
+            modifier = modifier
+                .size(64.dp)
+                .padding(8.dp)
+                .clip(
+                    RoundedCornerShape(50)
+                ),
+            contentScale = ContentScale.Crop,
+            painter = painterResource(dogIcon),
+            /*
+             * Content Description is not needed here - image is decorative, and setting a null content
+             * description allows accessibility services to skip this element during navigation.
+             */
+            contentDescription = null
+        )
+    }
+
+
+
+    @Composable
+    fun DogInformation(@StringRes dogName: Int, dogAge: Int, modifier: Modifier = Modifier) {
+        Column {
+            Text(
+                text = stringResource(dogName),
+                style = MaterialTheme.typography.h2,
+                modifier = modifier.padding(top = 8.dp)
+            )
+            Text(
+                text = stringResource(R.string.years_old, dogAge),
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+
+@Preview
+@Composable
+fun DarkThemePreview() {
+    DogAppTheme(darkTheme = true) {
+        WoofApp()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DogAppTheme {
+       WoofApp()
+    }
+}
---
+++ app/src/main/java/com/esosaphilip/dogapp/ui/theme/Theme.kt
@@ -0,0 +1,54 @@
+package com.esosaphilip.dogapp.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
+
+
+private val LightColorPalette = lightColors(
+    background = Green100,
+    surface = Green50,
+    onSurface = Grey900,
+    primary = Grey50,
+    onPrimary = Grey900,
+    secondary = Grey700
+)
+private val DarkColorPalette = darkColors(
+    background = Cyan900,
+    surface = Cyan700,
+    onSurface = White,
+    primary = Grey900,
+    onPrimary = White,
+    secondary = Grey100
+)
+
+
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+
+
+@Composable
+fun DogAppTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
